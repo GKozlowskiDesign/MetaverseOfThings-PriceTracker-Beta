63bdae5a535b39d18b78d98f4fa7b73fc8967712
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-27 11:46:52 -0500

    Update HistoryChart.jsx

--- src/componets/HistoryChart.jsx
+++ src/componets/HistoryChart.jsx
@@ -1,7 +1,7 @@
 
 
 import { useParams } from "react-router-dom";
-import useAxios from "../hooks/useAxios"
+import useAxios from "../Hooks/useAxios"
 import {
   Chart as ChartJS,
   CategoryScale,
