428243018868969a865541d1c0c6ba96207d87c7
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-12-27 13:01:38 -0500

    Update Navbar.jsx

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
         <div className='wrapper-container w-full'>
             <div className='flex items-center gap-1 cursor-pointer mb-10'>
                 <LogoIcon />
-                <h1 className='font-semibold text-3xl'><span className='text-3xl text-indigo-600'>MV</span>oT<span 
-                className='text-3xl text-indigo-600'>Ex</span>plorer</h1>   
+                <h1 className='font-semibold text-3xl'><span className='text-3xl text-indigo-600'>Crypto</span>Currency<span 
+                className='text-3xl text-indigo-600'>In</span>sights</h1>   
             </div>
         </div>
     </div>
