460298bed5d489958410d1f02c86a8ba14169e7d
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-06 12:50:24 -0500

    Update Hero.jsx

--- src/componets/Hero.jsx
+++ src/componets/Hero.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 const Hero = () => {
 return (
 <div>
-    <div name="about" className='w-full h-100 bg-gradient-to-r from-black via-indigo-900 to-black'>
-         <div class="w-full h-full  max-w-screen-lg p-6 mx-auto flex flex-col justify-center gap-2 ">
-            <section className='pt-10 py-40 flex items-center h-100 justify-center'>
+    <div name="about" className='w-full h-100 bg-black'>
+         <div class="w-full h-full  max-w-screen-lg p-20 mx-auto flex flex-col justify-center gap-2 ">
+            <section className='py-16 flex items-center h-100 justify-center'>
                 <div className="mx-auto max-w-[27.5rem]">
                 <div className="text-center">
-                <div className="pt-2  flex items-center justify-center ">
-                    <img className="h-30" src="https://github.com/gkozlowskidesign/React-MVoT-Website/blob/codespace-gkozlowskidesign-opulent-chainsaw-59jvrpj6p56fp7w5/src/assets/metaverseofthings_logo.png?raw=true" alt="MVoT"/> 
-                </div>     
-                    <h1 className="mt-2 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-white">Metaverse <br></br>
-                    <span className='text-black'>of Things</span></h1>    
+                <h1 className="mt-20 mb-2 text-[4rem] font-bold leading-[4rem]font-extrabold text-transparent text-7xl bg-clip-text bg-gradient-to-r from-purple-700 to-pink-300">Metaverse <br></br>
+                <span className='bg-clip-text bg-gradient-to-r from-purple-700 to-pink-300'>of Things</span></h1>
+                <h3 className="mb-2 text-[1rem] font-bold leading-8 text-white">Explore the Destination of Discovery!</h3>      
                 </div>
                 </div>
             </section> 
