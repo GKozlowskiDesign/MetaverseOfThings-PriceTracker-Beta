87079c2cbb52d87567c6b2ff7fe1aaa9dbde2c1a
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-06 18:13:08 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
       <Route path="/" element={<CryptoHome />} />
       <Route path="/coin/:id" element={<CryptoDetail />} />
     </Routes>
+    <Footer/>
     </BrowserRouter>
   )
 }
