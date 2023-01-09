439ed9762a63d7025ff6126f04bfcd04f2bbe3fc
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-09 14:41:31 -0500

    Update CryptoDetail.jsx

--- src/pages/CryptoDetail.jsx
+++ src/pages/CryptoDetail.jsx
@@ -1,15 +1,20 @@
 import CoinDetail from "../componets/CoinDetail"
 import HistoryChart from "../componets/HistoryChart"
-
-
-
+import CoinInfo from "../componets/CoinInfo"
 const CryptoDetail = () => {
   return (
-    <div className="bg-black w-full">  
-    <div className="wrapper-container pt-10 bg-black w-full">
-     <HistoryChart />
-     <CoinDetail />
-    </div>
+    <div className="bg-black w-full p-4"> 
+        <div className="wrapper-container mx-auto bg-black w-full">
+        <CoinDetail />
+        </div>
+       
+        <div className="wrapper-container mx-auto bg-black w-full">
+        <HistoryChart/>
+        </div>
+
+        <div className="wrapper-container mx-auto bg-black w-full">
+        <CoinInfo/>
+        </div>
     </div>
   
   )
