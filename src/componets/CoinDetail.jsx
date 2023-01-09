d0f38eb19511a653670ef682e7d0681eeb61b24f
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-08 20:16:31 -0500

    Update CoinDetail.jsx

--- src/componets/CoinDetail.jsx
+++ src/componets/CoinDetail.jsx
@@ -8,18 +8,19 @@ const CoinDetail = () => {
   if(!response) {
     return (
       <div className="wrapper-container bg-black">
+      <h1 className='text-2xl capitalize font-bold text-white mb-20'>{response.name}</h1>
         <Skeleton className="h-8 w-32 mb-4" />
         <Skeleton className="h-72 w-full mb-10" />
       </div>
     )
   }
   return (
-    <div className='bg-black'>
+    <div className='bg-black mb-20'>
       <div className='flex gap-2 items-center'>
         <img src={response.image.small} alt={response.name} />
         <h1 className='text-2xl capitalize font-bold text-white'>{response.name}</h1>
       </div>   
-      <div class="p-5 font-light ring-4 ring-gray-900 rounded-xl dark:border-gray-700 dark:bg-gray-900">
+      <div class="p-5 font-light ring-2 ring-gray-900 rounded-xl dark:border-gray-700 dark:bg-gray-900">
          <p className='mt-6 text-gray-500 [&>a]:text-blue-600 [&>a]:underline' dangerouslySetInnerHTML={{ __html: response.description.en }}></p> 
       </div>   
     </div>
