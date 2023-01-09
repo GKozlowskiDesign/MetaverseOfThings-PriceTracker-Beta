52900eb951a2e544331e547215933f05e499c151
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-08 20:11:24 -0500

    Update CoinDetail.jsx

--- src/componets/CoinDetail.jsx
+++ src/componets/CoinDetail.jsx
@@ -1,9 +1,6 @@
 import { useParams } from 'react-router-dom';
 import useAxios from '../Hooks/useAxios';
 import Skeleton from './Skeleton';
-
-
-
 const CoinDetail = () => {
   const { id } = useParams();
   const { response } = useAxios(`coins/${id}?localization=false&tickers=false&market_data=false&community_data=false&sparkline=false`);
@@ -22,13 +19,10 @@ const CoinDetail = () => {
       <div className='flex gap-2 items-center'>
         <img src={response.image.small} alt={response.name} />
         <h1 className='text-2xl capitalize font-bold text-white'>{response.name}</h1>
-      </div>  
-
-          
-              <div class="p-5 font-light border border-b-0 border-gray-900 dark:border-gray-700 dark:bg-gray-900">
-              <p className='mt-6 text-gray-500 [&>a]:text-blue-600 [&>a]:underline' dangerouslySetInnerHTML={{ __html: response.description.en }}></p> 
-              </div>
-       
+      </div>   
+      <div class="p-5 font-light ring-4 ring-gray-900 rounded-xl dark:border-gray-700 dark:bg-gray-900">
+         <p className='mt-6 text-gray-500 [&>a]:text-blue-600 [&>a]:underline' dangerouslySetInnerHTML={{ __html: response.description.en }}></p> 
+      </div>   
     </div>
   )
 }
