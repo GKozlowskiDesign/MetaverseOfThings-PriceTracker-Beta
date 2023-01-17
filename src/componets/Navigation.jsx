0ac45ffd11858353ff64c6ea7b8189d18422e9f8
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-17 00:13:49 -0500

    Update Navigation.jsx

--- src/componets/Navigation.jsx
+++ src/componets/Navigation.jsx
@@ -18,92 +18,91 @@ return (
 		<ul className="pl-4">
 			
 {/* Home Navigation Icon */}
-			<li class="ring-2 ring-black hover:ring-white hover:text-white mt-20 rounded-xl">
-					<a
-					href="https://metaverseot.xyz/"
-					class="h-16 px-6 flex flex justify-center items-center w-full
-					focus:text-white">
-					<svg xmlns="http://www.w3.org/2000/svg" 
-					fill="none" viewBox="0 0 24 24" 
-					stroke-width="1.5" 
-					stroke="currentColor" 
-					class="w-6 h-6">
-					<path stroke-linecap="round" 
-					stroke-linejoin="round" 
-					d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 
-					12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 
-					1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 
-					0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-					</svg>
-					</a>
-			</li>
+	{/* Navigation Icon (Home)*/}
+	<p className='text-xs text-center invisible lg:hidden'>Home</p>
+	<button class="hover:bg-indigo-600 p-2 bg-gray-900 ring-2 ring-gray-600 text-gray-600 text-center justify-center rounded-lg mx-auto cursor-pointer">
+			<div class="text-center justify-center content-center mx-auto">
+			<a
+			href="/"
+			class=" justify-center items-center w-full
+					focus:text-orange-500">
+				<svg xmlns="http://www.w3.org/2000/svg" 
+				fill="none" viewBox="0 0 24 24" 
+				stroke-width="1.5" 
+				stroke="currentColor" 
+				class="w-8 h-8">
+  				<path stroke-linecap="round" 
+				stroke-linejoin="round" 
+				d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 
+				12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 
+				1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 
+				0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+				</svg>
+			</a>
+			</div>
+			</button>
 {/* Dashboard Navigation Icon */}
-			<li class="ring-2 ring-black hover:ring-white hover:text-white mt-4 rounded-xl">
-					<a
-					href="https://metaverseot.xyz/foundation"
-					class="h-16 px-6 flex flex justify-center items-center w-full
-					focus:text-white">
-					<svg xmlns="http://www.w3.org/2000/svg" 
-					fill="none"
-					viewBox="0 0 24 24" 
-					stroke-width="1.5" 
-					stroke="currentColor" 
-					class="w-6 h-6">
-					<path stroke-linecap="round" 
-					stroke-linejoin="round" 
-					d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 
-					2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 
-					0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 
-					2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 
-					2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z" />
-					</svg>
-					</a>
-			</li>
+		{/* Navigation Icon (Trade)*/}
+	<p className='text-xs invisible lg:hidden'>Trade</p>
+			<button class="hover:bg-indigo-600 p-2 bg-gray-900 ring-2 ring-gray-600 text-gray-600 text-center justify-center rounded-lg mx-auto cursor-pointer">
+			<div class="text-center justify-center content-center mx-auto">
+			<a
+			href="/tokens"
+			class=" justify-center w-full hover:text-white
+				focus:text-orange-500">
+				<svg xmlns="http://www.w3.org/2000/svg" 
+				fill="none" viewBox="0 0 24 24" 
+				stroke-width="1.5" 
+				stroke="currentColor" 
+				class="w-8 h-8">
+  				<path stroke-linecap="round" 
+				stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 
+				0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 
+				0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+				</svg>
+			</a>
+			</div>
+			</button>
 {/* Learning Navigation Icon */}
-			<li class="ring-2 ring-black hover:ring-white hover:text-white mt-4 rounded-xl">
-					<a
-					href="https://metaverseot.xyz/learning"
-					class="h-16 px-6 flex flex justify-center items-center w-full
-					focus:text-white">
-					<svg xmlns="http://www.w3.org/2000/svg" 
-					fill="none" 
-					viewBox="0 0 24 24" 
-					stroke-width="1.5" 
-					stroke="currentColor" 
-					class="w-6 h-6">
-					<path stroke-linecap="round" 
-					stroke-linejoin="round" 
-					d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 
-					20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 
-					0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 
-					5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 
-					0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 
-					8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
-					</svg>
-					</a>
-			</li>
+		{/* Navigation Icon (Learn)*/}
+			<p className='text-xs  invisible lg:hidden'>Learn</p>
+			<button class="hover:bg-indigo-600 p-2 bg-gray-900 ring-2 ring-gray-600 text-gray-600 text-center justify-center rounded-lg mx-auto cursor-pointer">
+			<div class="text-center justify-center content-center mx-auto">
+			<a
+			href="/blog"
+			class=" justify-center items-center w-full focus:text-orange-500">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" 
+				viewBox="0 0 24 24" stroke-width="1.5" 
+				stroke="currentColor" 
+				class="w-8 h-8">
+  				<path stroke-linecap="round" stroke-linejoin="round"
+				d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 
+				21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+				</svg>
+			</a>
+			</div>
+			</button>
+
 {/* Chart Navigation Icon */}
-			<li class="ring-2 ring-black hover:ring-white hover:text-white mt-4 rounded-xl">
-					<a
-					href="https://metaverseofthings-analyitcs.netlify.app/"
-					class="h-16 px-6 flex flex justify-center items-center w-full
-					focus:text-white">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" 
-					viewBox="0 0 24 24" 
-					stroke-width="1.5" 
-					stroke="currentColor" 
-					class="w-6 h-6">
- 					<path stroke-linecap="round" 
-					stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 
-					1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 
-					19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0
-					.621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 
-					1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 
-					0 01-1.125-1.125V4.125z" />
-					</svg>
-				</a>
-			</li>
-		</ul>
+	{/* Navigation Icon (Logout)*/}
+			<p className='text-xs invisible lg:hidden'>Logout</p>
+			<button class="hover:bg-indigo-600 p-2 bg-gray-900 ring-2 ring-gray-600 mr-6 text-gray-600 text-center justify-center rounded-lg mx-auto cursor-pointer">
+			<div class="text-center justify-center content-center mx-auto">
+			<a
+			href="https://metaverseot.xyz/"
+			class=" justify-center items-center w-full focus:text-orange-500">
+				<svg xmlns="http://www.w3.org/2000/svg" 
+				fill="none" viewBox="0 0 24 24" 
+				stroke-width="1.5" 
+				stroke="currentColor" 
+				class="w-8 h-8">
+				<path stroke-linecap="round" stroke-linejoin="round" 
+				d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 
+				21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+				</svg>
+			</a>
+			</div>
+			</button>	
 </aside>
 {/* Navigation Title Text */}
 		<div class="mx-auto flex flex-col">
