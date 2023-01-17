d5c8cfff557257f8ea1428e40279b6e015db045a
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-17 17:02:09 -0500

    Update Navigation.jsx

--- src/componets/Navigation.jsx
+++ src/componets/Navigation.jsx
@@ -8,31 +8,13 @@ return (
 {/* Navigation */}
 			
 
-	{/* Navigation Icon (Home)*/}
-	<button class="p-2 blur  bg-gray-600  ring-2 ring-gray-600 text-indigo-200 text-center justify-center rounded-lg mx-auto cursor-default">
-			<div class="text-center justify-center content-center mx-auto">
-			<svg xmlns="http://www.w3.org/2000/svg" 
-				fill="none" viewBox="0 0 24 24" 
-				stroke-width="1.5" 
-				stroke="currentColor" 
-				class="w-8 h-8">
-  				<path stroke-linecap="round" 
-				stroke-linejoin="round" 
-				d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 
-				12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 
-				1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 
-				0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
-				</svg>
-			</div>
-			</button>
-
 
 
 		{/* Navigation Icon (Trade)*/}
 			<button class="hover:bg-indigo-600 p-2  bg-purple-600  ring-2 ring-gray-600 text-indigo-200 text-center justify-center rounded-lg mx-auto cursor-pointer">
 			<div class="text-center justify-center content-center mx-auto">
 			<a
-			href="/tokens"
+			href="/"
 			class=" justify-center w-full hover:text-white
 			focus:text-white">
 			<svg xmlns="http://www.w3.org/2000/svg" 
