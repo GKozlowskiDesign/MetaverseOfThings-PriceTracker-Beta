7694e9ebe57a71552d70723590786f8786a25108
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-17 00:35:57 -0500

    Update CryptoHome.jsx

--- src/pages/CryptoHome.jsx
+++ src/pages/CryptoHome.jsx
@@ -1,10 +1,12 @@
 import Markets from "../componets/Markets"
 import Trending from "../componets/Trending"
+import Navigation from "../componets/Navigation
 
 const CryptoHome = () => {
     return (
-        <div className="bg-gradient-to-b from-black via-purple-900 to-indigo-700">
-        <div className="wrapper-container ">
+      <div className="p-2 bg-gradient-to-r from-black via-gray-900 to-indigo-900"> 
+        <Navbar/>
+            <div className="wrapper-container p-4 Profile-center">
             <Trending />
             <Markets />
         </div>
