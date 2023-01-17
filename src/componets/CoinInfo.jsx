0b6a6ce488974f88951d2af4b231f90158694874
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-01-17 00:09:57 -0500

    Update CoinInfo.jsx

--- src/componets/CoinInfo.jsx
+++ src/componets/CoinInfo.jsx
@@ -13,25 +13,76 @@ const CoinInfo = () => {
       <div className="wrapper-container">
         <Skeleton className="h-8 w-32 mb-4" />
         <Skeleton className="h-72 w-full mb-10" />
+        <Skeleton className="h-72 w-full mb-10" />
+        <Skeleton className="h-72 w-full mb-10" />
+        <Skeleton className="h-72 w-full mb-10" />
       </div>
     )
   }
 
   return (
-    <div className='pb-20'>
-  
-<div id="accordion-collapse" data-accordion="collapse" className='p-8 ring-gray-900 bg-gray-900 ring-4 rounded-xl'>
-<h1 className='text-white text-xl justify-center rounded p-2 text-left'>Description</h1>
+    <div className='pt-2 mx-auto p-2 mb-20'>
 
-        <div class="font-lightrounded-xl">
-        <p className='mt-2 p-2 rounded-xl text-base  text-gray-500 [&>a]:text-blue-600 [&>a]:underline' dangerouslySetInnerHTML={{ __html: response.description.en }}></p> 
+ <div className='mx-auto bg-gray-900  rounded-xl shadow-xl ring-2 ring-gray-600'>
+    <div class="font-light rounded-xl mx-auto rounded-xl">
+        <h3 className='text-3xl text-white p-4 mx-auto pt-6'>
+        Description  
+        <span className="text-xs text-purple-500">  
+          <br></br>
+        Coingecko API Real-Time
+        </span>
+        </h3>
+
+
+    <div className=' pb-4 p-4 mx-auto bg-black rounded-xl ring-2 ring-gray-600'>
+      <ul className='mx-auto'>
+      <li className='mt-2 p-2 rounded-xl  text-center mx-auto
+      text-base text-gray-500 [&>a]:text-blue-600 [&>a]:underline' dangerouslySetInnerHTML={{ __html: response.description.en }}>
+      </li>
+      </ul>      
+      </div>
+     </div>
+   </div>
+
+
+      <div className='mx-auto bg-gray-900  rounded-xl shadow-xl ring-2 ring-gray-600 mt-8'>
+    <div class="font-light rounded-xl mx-auto">
+    <h3 className='text-3xl text-white p-4 pb-8 pt-10 mx-auto'>Project Information    <span className="text-xs text-purple-500">  
+        <br></br>
+        Coingecko API Real-Time
+        </span></h3>
+    <div className=' pb-8 p-4 mx-auto bg-black rounded-xl ring-2 ring-gray-600'>
+      <ul className='mx-auto'>
+
+
+      <h3 className='text-xl text-white p-4'>Contract Address</h3>
+         <li className=' p-4 rounded-xl  hover:text-white mx-auto
+           text-xs  text-gray-500 [&>a]:text-blue-600 [&>a]:underline'  dangerouslySetInnerHTML={{ __html: response.contract_address }} 
+             onClick={() => {navigator.clipboard.writeText(this.state.textToCopy)}} >
+               </li>
+
+
+           <h3  className='text-lg text-white p-4'>Website Address</h3>  
+              <li className='p-4 rounded-xl hover:text-white mx-auto
+                 text-xs  text-gray-500 [&>a]:text-blue-600 [&>a]:underline' dangerouslySetInnerHTML={{ __html: response.links.homepage }}>
+                   </li>
+
+
+        <h3 className='text-lg text-white p-4'>Twitter Address</h3>
+           <li className='p-4 rounded-xl text-xs  text-gray-500 [&>a]:text-blue-600 [&>a]:underline'
+             dangerouslySetInnerHTML={{ __html: response.links.twitter_screen_name }}>
+               </li>
+    </ul>
+    </div>       
     </div>
-</div>
-</div>
+    </div>
+    </div>
   )
 }
 
 export default CoinInfo
+
+
 
 
 
