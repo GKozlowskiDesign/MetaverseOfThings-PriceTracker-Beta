b59a211958cf12c77672bd56e7ba3d854ec3fe07
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-06-21 20:19:41 -0400

    Update CryptoTokens.jsx

--- src/pages/CryptoTokens.jsx
+++ src/pages/CryptoTokens.jsx
@@ -1,11 +1,14 @@
 import Markets from "../componets/Markets"
 import Navigation from "../componets/Navigation"
+import Hero from "../componets/Hero"
 
 
 const CryptoTokens = () => {
     return (
         <div className="bg-gradient-to-r from-black via-gray-900 to-indigo-900 p-6">
+            
               <Navigation/>
+                <Hero />
         <div className="wrapper-container p-12 mx-auto">
             <Markets />
         </div>
