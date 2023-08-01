a0d2b00f560c94de8ec8332328840eff35c2e6ba
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-08-01 11:18:11 -0400

    Update CryptoDetail.jsx

--- src/pages/CryptoDetail.jsx
+++ src/pages/CryptoDetail.jsx
@@ -1,11 +1,9 @@
 import CoinDetail from "../componets/CoinDetail"
 import HistoryChart from "../componets/HistoryChart"
 import CoinInfo from "../componets/CoinInfo"
-import Navigation from "../componets/LandingPage"
 const CryptoDetail = () => {
   return (
     <div className="bg-gradient-to-r from-black via-gray-900 to-indigo-900 w-full p-6"> 
-          <Navigation/>
         <div className="wrapper-container mx-auto w-full">
         <CoinDetail />
         </div>
