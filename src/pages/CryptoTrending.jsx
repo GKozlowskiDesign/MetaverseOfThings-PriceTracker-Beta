707fb6c83cec135d4b03fd7b36fa43203ea21714
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-09-04 12:08:35 -0400

    Update CryptoTrending.jsx

--- src/pages/CryptoTrending.jsx
+++ src/pages/CryptoTrending.jsx
@@ -4,7 +4,7 @@ import Navigation from "../componets/LandingPage"
 
 const CryptoTokens = () => {
     return (
-        <div className="bg-gradient-to-r from-black via-gray-900 to-indigo-900 p-6">
+        <div className="bg-gradient-to-r from-black via-indigo-900 to-pink-900 p-6">
         <Navigation/>
             <div className="wrapper-container p-2 mx-auto">
             <Trending />
