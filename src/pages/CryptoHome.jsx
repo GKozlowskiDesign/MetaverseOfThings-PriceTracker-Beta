903fdcc3555e43d424fc82e2f6d401d6cd284b4a
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-09-04 12:05:41 -0400

    Update CryptoHome.jsx

--- src/pages/CryptoHome.jsx
+++ src/pages/CryptoHome.jsx
@@ -3,7 +3,7 @@ import Trending from "../componets/Trending"
 
 const CryptoHome = () => {
     return (
-        <div className="bg-gradient-to-r from-black via-gray-900 to-indigo-900 p-6">
+        <div className="bg-gradient-to-r from-black via-indigo-900 to-pink-900 p-6">
         <Navigation/>
             <div className="wrapper-container p-12 mx-auto">
         <Trending/>
